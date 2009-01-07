eceb94d61b86e4cfad8052de061c7468d5f8960c
Luke Randall <luke.randall@gmail.com>
2009-01-07 10:47:42 +0200

    Fixes to get it to actually work
     * actionpack helper calls must be bound to @template
     * cleaned up the rdoc a bit more

--- lib/simple_fields/simple_field_helpers.rb
+++ lib/simple_fields/simple_field_helpers.rb
@@ -4,19 +4,24 @@ module SimpleFields
   # a field type, a label to override the default, and an HTML class
   module SimpleFieldHelpers
     # Generates a label, field, description and tooltip for the specified attribute.
-    # Specify +:field_type+ if you need something other than a text field, +:label+ to override
-    # the default label, and +:class+ to provide an HTML class.
+    # Specify +field_type+ if you need something other than a text field, +label+ to override
+    # the default label, and +class+ to provide an HTML class.
     # The description and explanation lookup uses the I18n gem (which ships with Rails >= 2.2)
     def field(field_name, options = {})
       options.reverse_merge! :field_type => :text_field, :label => nil, :class => nil
       key = "#{self.object_name}_#{field_name}_description".to_sym
       html = []
       html << "<p>"
+      html << "\n"
       html << self.label(*(options[:label] ? [field_name, options[:label]] : [field_name]))
+      html << "\n"
       html << self.send(options[:field_type], field_name, :class => options[:class])
       html << tooltip(field_name)
-      html << content_tag(:span, t(key), :class => 'description') if localization_exists(key)
+      html << "\n"
+      html << @template.content_tag(:span, @template.t(key), :class => 'description') if localization_exists(key)
+      html << "\n"
       html << "</p>"
+      html << "\n"
       html
     end
 
@@ -29,16 +34,21 @@ module SimpleFields
       link = "#{field}_link"
       explanation = key.to_s
       html = []
-      html << link_to(image_tag('explanation.png', :class => 'explanation'), '#', :id => link)
-      html << content_tag(:span, textilize_without_paragraph(t(key)), :class => 'tooltip', :id => explanation)
-      html << "<script type=\"text/javascript\">
-        var #{explanation}_tooltip = new Tooltip('#{link}', '#{explanation}')
-      </script>"
+      html << @template.link_to(@template.image_tag('explanation.png', :class => 'explanation'), '#', :id => link)
+      html << "\n"
+      html << @template.content_tag(:span, @template.textilize_without_paragraph(@template.t(key)), :class => 'tooltip', :id => explanation)
+      html << "\n"
+      html << "<script type=\"text/javascript\">"
+      html << "\n"
+      html << "  var #{explanation}_tooltip = new Tooltip('#{link}', '#{explanation}')"
+      html << "\n"
+      html << "</script>"
+      html << "\n"
     end
 
     # Determines if a localized string for +key+ exists
     def localization_exists(key)
-      t(key, :default => 'not_found') != 'not_found'
+      @template.t(key) != '-Needs definition-'
     end
   end
 end
